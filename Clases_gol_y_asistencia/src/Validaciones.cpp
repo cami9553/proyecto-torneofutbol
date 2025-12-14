586ed4de097b85f16eb8fae97c3aeda9b16aecf8
maria rosa morales <156377897+moralesmr@users.noreply.github.com>
2025-12-14 18:23:38 -0300

    agregado de validaciones de numero rango y si/no
    Added functions to validate integer input within a range and to read a yes/no option.

--- Clases_gol_y_asistencia/src/Validaciones.cpp
+++ Clases_gol_y_asistencia/src/Validaciones.cpp
@@ -20,3 +20,34 @@ int leerEntero() {
         }  
       }
 }  
+
+
+// validacion para leer un entero en un rango dado
+int leerEnteroEnRango(int min, int max) {
+    int valor;
+    do {
+        valor = leerEntero();
+        if (valor < min || valor > max) {
+            cout << "Error: El valor debe estar entre " << min << " y " << max << ". Intente nuevamente: ";
+        }
+    } while (valor < min || valor > max);
+    return valor;
+}
+
+// validacion para opcion si o no (S/N)
+char leerOpcionSN() {
+    char opcion;
+    string entrada;
+    
+    while (true) {
+        getline(cin, entrada);
+        
+        if (entrada.length() == 1) {
+            opcion = toupper(entrada[0]);
+            if (opcion == 'S' || opcion == 'N') {
+                return opcion;
+            }
+        }
+        cout << "Error: Debe ingresar 'S' o 'N'. Intente nuevamente: ";
+    }
+}
