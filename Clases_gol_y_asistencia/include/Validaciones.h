b04f448e4f3cbf1e5a02d43e9f61da05f6a38da5
maria rosa morales <156377897+moralesmr@users.noreply.github.com>
2025-12-20 20:31:23 -0300

    Change leerNombreApellido to return a boolean

--- Clases_gol_y_asistencia/include/Validaciones.h
+++ Clases_gol_y_asistencia/include/Validaciones.h
@@ -6,7 +6,7 @@ int leerEnteroConIntentos(int maxIntentos = 3);
 int leerEnteroEnRango(int min, int max);
 char leerOpcionSN();
 bool validarSoloLetrasEspacios(const char* texto);
-void leerNombreApellido(char* campo, int tamano, const char* mensaje);
+bool leerNombreApellido(char* campo, int tamano, const char* mensaje);
 void leerTextoValido(char* campo, int tamano, const char* mensaje);
 bool validarTelefono(const char* texto, int minDigitos, int maxDigitos);
 void leerTelefono(char* telefono, int tamano, int minDigitos, int maxDigitos);
