382a8752765b39b16b3664f6acb93f7ef0c86cbb
danPrueba <ibaniez.danilo2@gmail.com>
2025-12-23 17:21:34 -0300

    fix: listar Jugadores

--- Clases_gol_y_asistencia/src/MenuOperativo.cpp
+++ Clases_gol_y_asistencia/src/MenuOperativo.cpp
@@ -26,8 +26,7 @@ void menuOperativo()
         cout << "1.Registrar Partido\n";
         cout << "2.Ver tabla posiciones\n";
         cout << "3.Ver tabla goleadores\n";
-        cout << "4.Ver tabla asistidores\n";
-        cout << "5.Ver Fixture\n";
+        cout << "4.Ver Fixture\n";
         cout << "0.Salir\n";
         cout << "Seleccione una opcion:";
 
@@ -218,97 +217,7 @@ void menuOperativo()
             break;
 
         }
-
         case 4:
-        {
-
-            cout << "\n=== TABLA DE ASISTIDORES ===\n";
-            Archivoasistencia archivoAsistencias("asistencias.dat");
-            ArchivoJugador archivoJugadores("jugadores.dat");
-
-            archivoAsistencias.cargarDesdeArchivo();
-            int cantAsistencias = archivoAsistencias.getCantidad();
-            int cantJugadores = archivoJugadores.getCantidadRegistros();
-
-            if (cantAsistencias == 0)
-            {
-                cout << "No hay asistencias registradas.\n";
-                cin.get();
-                break;
-            }
-
-            int* asistenciasPorJugador = new int[cantJugadores]();
-            int* dnis = new int[cantJugadores]();
-
-            // Guardar los DNI de los jugadores
-            for (int i = 0; i < cantJugadores; i++)
-            {
-                Jugador j = archivoJugadores.leerRegistro(i);
-                dnis[i] = j.getDni();
-            }
-
-            // Contar asistencias
-            for (int i = 0; i < cantAsistencias; i++)
-            {
-                Asistencia a = archivoAsistencias.getAsistencia(i);
-                int dniAsistente = a.getDniJugador();
-                for (int j = 0; j < cantJugadores; j++)
-                {
-                    if (dnis[j] == dniAsistente)
-                    {
-                        asistenciasPorJugador[j]++;
-                        break;
-                    }
-                }
-            }
-
-            // Ordenar por cantidad de asistencias (descendente)
-            int* indices = new int[cantJugadores];
-            for (int i = 0; i < cantJugadores; i++) indices[i] = i;
-
-            for (int i = 0; i < cantJugadores - 1; i++)
-            {
-                for (int j = i + 1; j < cantJugadores; j++)
-                {
-                    if (asistenciasPorJugador[indices[j]] > asistenciasPorJugador[indices[i]])
-                    {
-                        int temp = indices[i];
-                        indices[i] = indices[j];
-                        indices[j] = temp;
-                    }
-                }
-            }
-
-            cout << "----------------------------------------\n";
-            cout << "POS\tJUGADOR\t\tASISTENCIAS\n";
-            cout << "----------------------------------------\n";
-
-            int posicion = 1;
-            for (int i = 0; i < cantJugadores; i++)
-            {
-                int idx = indices[i];
-                if (asistenciasPorJugador[idx] > 0)
-                {
-                    Jugador j = archivoJugadores.leerRegistro(idx);
-                    cout << posicion << "\t"
-                         << j.getApellido() << " " << j.getNombre()
-                         << "\t" << asistenciasPorJugador[idx] << endl;
-                    posicion++;
-                }
-            }
-
-            cout << "----------------------------------------\n";
-
-            delete[] asistenciasPorJugador;
-            delete[] dnis;
-            delete[] indices;
-
-            cin.get();
-            break;
-
-        }
-
-        case 5:
         {
             ArchivoPartido archPartido("partidos.dat");
             archPartido.listarRegistros();
@@ -328,16 +237,3 @@ void menuOperativo()
 
 
 }
-
-/*void registrarAsistencia() {
-    Archivoasistencia archivo("asistencias.dat");
-
-    Asistencia a;
-    cout << "\n=== REGISTRAR ASISTENCIA ===\n";
-    a.cargar(); // el método cargar() debería pedir dniJugador, idPartido.
-    archivo.cargarDesdeArchivo();  // carga las que ya hay
-    archivo.agregarasistencia(a);  // suma la nueva
-    archivo.guardarEnArchivo();    // guarda todo
-    cout << "Asistencia registrada correctamente.\n";
-    cin.get();
-}*/
